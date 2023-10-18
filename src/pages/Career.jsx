5f320b3145a0d095a306d779bbf6ea320a9b4a29
Brigita Bizjak <brigita.bizjak05@gmail.com>
2023-10-18 22:10:09 +0200

    styling

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 const Career = () => {
   return (
-    <div className="text-black min-h-screen flex flex-col items-center justify-center">
+    <div className="text-black min-h-screen flex flex-col items-center justify-center bg-[#f8f4f4]">
       <div className="text-center mb-12">
         <h1 className="text-4xl font-bold mt-7">
           Join Us in Shaping the Future of Technology
@@ -13,84 +14,131 @@ const Career = () => {
           enhance the homes and lives of our customers.
         </p>
         <p className="mt-4">
-          <Link
-            to="/career/open-roles"
-            className="text-gray text-xl"
-          >
-            View open positions <span className='underline hover:opacity-40'> here.</span>
+          <Link to="/career/open-roles" className="text-gray text-xl">
+            View open positions{' '}
+            <span className="underline hover:opacity-40"> here.</span>
           </Link>
         </p>
       </div>
 
-      <div className="ml-4 mr-8 bg-slate-100 ">
-        <h2 className="text-3xl text-bold">Our core values are:</h2>
-        <div className="grid grid-cols-3 gap-1 mt-4 text-xl">
-          {/* Value 1 */}
-          <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
-            <div className="bg-white rounded-lg p-6 shadow-md">
-              <p className="hover:opacity-100 cursor-pointer z-10">
-                <strong> Innovation and Impact </strong>
-                We are dedicated to pioneering innovation that leaves a lasting,
-                positive impact on the future of humanity. Our commitment is to
-                create solutions that push the boundaries of what's possible and
-                improve the lives of people worldwide. Anything that doesn't
-                align with this vision is considered outside our scope.
-              </p>
-            </div>
-          </div>
-
-          {/* Value 2  */}
-          <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
-            <div className="bg-white rounded-lg p-6 shadow-md">
-              <p className="hover:opacity-100 cursor-pointer z-10">
-                <strong> Intense and Scrappy   </strong>
-                We are dedicated to pioneering innovation that leaves a lasting,
-                positive impact on the future of humanity. Our commitment is to
-                create solutions that push the boundaries of what's possible and
-                improve the lives of people worldwide. Anything that doesn't
-                align with this vision is considered outside our scope.
-              </p>
-            </div>
-          </div>
-
-          {/* Value 3 */}
-          
-          <div>
-            <strong>Scale:</strong> We believe that scale is magical. It extends
-            not only to the size of our models, systems, and processes but also
-            to our personal growth and ambitions. When faced with uncertainty,
-            our instinct is to scale up, as it enables us to reach new heights
-            and overcome challenges.
-          </div>
-          <div>
-            <strong>Make Something People Love:</strong> We are driven by the
-            aspiration to create technology and products that have a profoundly
-            positive and transformative impact on people's lives. Our goal is to
-            develop solutions that are not just functional but deeply loved by
-            the individuals they serve.
-          </div>
-          <div>
-            <strong>Team Spirit:</strong> Our greatest achievements and unique
-            contributions stem from effective collaboration within and between
-            teams. We take responsibility for solving problems and recognize
-            that no issue is solely someone else's concern.
-          </div>
-          <div>
-            <strong>True Flexibility:</strong> We promote an environment of true
-            flexibility, offering the freedom for individuals to adapt to their
-            unique work styles. Creative solutions often emerge when people have
-            the autonomy to innovate and make decisions that drive progress.
-          </div>
-        </div>
-      </div>
-
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-3xl mt-8">
-        <p className="text-2xl font-bold mb-4">When Dreams Become Reality</p>
+      <div className="bg-white p-8 rounded-lg shadow-lg max-w-3xl mt-8 flex space-x-3 mb-8">
         <img
           src="https://images.unsplash.com/photo-1600880292089-90a7e086ee0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
           alt="Collaboration"
+          className="w-full h-[350px] rounded-lg shadow-md pl-4"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1529310399831-ed472b81d589?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Collaboration"
           className="w-full h-[350px] rounded-lg shadow-md"
         />
+        <img
+          src="https://images.unsplash.com/photo-1635185481431-661b09594e6c?auto=format&fit=crop&q=80&w=2127&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Collaboration"
+          className="w-full h-[350px] rounded-lg shadow-md"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 h-max-[250px] mt-4">
+        <h2 className="col-span-3 ml-6 text-2xl font-bold text-center underline">
+          Our core values are
+        </h2>
+
+        {/* Value 1 */}
+
+        <div className="bg-white p-4 rounded-lg shadow-lg shadow-blue-500">
+          <p className="font-bold underline">Innovation and Impact</p>
+          <p className="pt-2}">
+            We are dedicated to pioneering innovation that leaves a lasting,
+            positive impact on the future of humanity. Our commitment is to
+            create solutions that push the boundaries of what's possible and
+            improve the lives of people worldwide. Anything that doesn't align
+            with this vision is considered outside our scope.
+          </p>
+        </div>
+
+        {/* Value 2  */}
+
+        <div className="bg-white p-4 rounded-lg shadow-lg shadow-blue-500">
+          <p className="font-bold">Intense and Scrappy</p>
+          <p className="pt-2">
+            We are dedicated to pioneering innovation that leaves a lasting,
+            positive impact on the future of humanity. Our commitment is to
+            create solutions that push the boundaries of what's possible and
+            improve the lives of people worldwide. Anything that doesn't align
+            with this vision is considered outside our scope.
+          </p>
+        </div>
+
+        {/* Value 3 */}
+
+        <div className="bg-white p-4 rounded-lg shadow-lg shadow-blue-500">
+          <p className="font-bold">Scale </p>
+          <p className="pt-2">
+            We believe that scale is magical. It extends not only to the size of
+            our models, systems, and processes but also to our personal growth
+            and ambitions. When faced with uncertainty, our instinct is to scale
+            up, as it enables us to reach new heights and overcome challenges.
+          </p>
+        </div>
+
+        <div className="bg-white p-4 rounded-lg shadow-lg shadow-blue-500">
+          <p className="font-bold">Make something people love </p>
+          <p className="pt-2">
+            We are driven by the aspiration to create technology and products
+            that have a profoundly positive and transformative impact on
+            people's lives. Our goal is to develop solutions that are not just
+            functional but deeply loved by the individuals they serve.
+          </p>
+        </div>
+
+        <div className="bg-white p-4 rounded-lg shadow-lg shadow-blue-500">
+          <p className="font-bold">Team Spirit </p>
+          <p className="pt-2">
+            Our greatest achievements and unique contributions stem from
+            effective collaboration within and between teams. We take
+            responsibility for solving problems and recognize that no issue is
+            solely someone else's concern.
+          </p>
+        </div>
+
+        <div className="bg-white p-4 rounded-lg shadow-lg shadow-blue-500">
+          <p className="font-bold">True Flexibility </p>
+          <p className="pt-2">
+            We promote an environment of true flexibility, offering the freedom
+            for individuals to adapt to their unique work styles. Creative
+            solutions often emerge when people have the autonomy to innovate and
+            make decisions that drive progress.
+          </p>
+        </div>
+        <div className="p-10 text-xl font-bold text-center">
+          <div className="pt-8">
+            <p>Our office in Amsterdam </p>
+            <div className="flex space-x-3 space-between ml-8 mt-4">
+              <img
+                className="h-max[250px] rounded-md"
+                src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="office"
+              />
+              <img
+                className="h-max[250px] rounded-md"
+                src="https://images.unsplash.com/photo-1497366754035-f200968a6e72?auto=format&fit=crop&q=80&w=2069&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="office"
+              />
+              <img
+                className="h-max[250px] rounded-md"
+                src="https://images.unsplash.com/photo-1575886876783-ea3cbbc8204d?auto=format&fit=crop&q=80&w=2034&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="office"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p className="text-center font-bold">
+          Hire & Admire LLC. All rights reseved. Created for learning purposes.
+          Images obtained on Unsplash.{' '}
+        </p>
       </div>
     </div>
   );
--- src/pages/OpenRoles.jsx
+++ src/pages/OpenRoles.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const OpenRoles = () => {
   return (
     <div className="bg-gradient-to-r from-blue-950 to-green-950 py-10 ">
-
       <div className="container mx-auto text-white">
         <h1 className="text-4xl font-bold text-center mb-4">
           Explore Exciting Open Roles
         </h1>
         <p className="text-lg text-center mb-8">
-          Join our dynamic team and help us shape the future of home repair and improvement.
+          Join our dynamic team and help us shape the future of home repair and
+          improvement.
         </p>
       </div>
 
@@ -17,9 +17,12 @@ const OpenRoles = () => {
         {/* Job 1 */}
         <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
           <div className="bg-white rounded-lg p-6 shadow-md">
-            <h2 className="text-xl font-bold text-gray-800 mb-2">Plumbing Specialist</h2>
+            <h2 className="text-xl font-bold text-gray-800 mb-2">
+              Plumbing Specialist
+            </h2>
             <p className="text-gray-600">
-              Are you a skilled plumber? Join our team to provide top-notch plumbing solutions to our customers.
+              Are you a skilled plumber? Join our team to provide top-notch
+              plumbing solutions to our customers.
             </p>
             <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
               Apply Now
@@ -29,9 +32,12 @@ const OpenRoles = () => {
         {/* Job 2 */}
         <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
           <div className="bg-white rounded-lg p-6 shadow-md">
-            <h2 className="text-xl font-bold text-gray-800 mb-2">Electrical Engineer</h2>
+            <h2 className="text-xl font-bold text-gray-800 mb-2">
+              Electrical Engineer
+            </h2>
             <p className="text-gray-600">
-              Join us as an electrical engineer and work on cutting-edge electrical projects for our clients.
+              Join us as an electrical engineer and work on cutting-edge
+              electrical projects for our clients.
             </p>
             <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
               Apply Now
@@ -41,9 +47,12 @@ const OpenRoles = () => {
         {/* Job 3 */}
         <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
           <div className="bg-white rounded-lg p-6 shadow-md">
-            <h2 className="text-xl font-bold text-gray-800 mb-2">Gardening Expert</h2>
+            <h2 className="text-xl font-bold text-gray-800 mb-2">
+              Gardening Expert
+            </h2>
             <p className="text-gray-600">
-              If you have a green thumb and a passion for gardening, we want you to join our gardening team.
+              If you have a green thumb and a passion for gardening, we want you
+              to join our gardening team.
             </p>
             <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
               Apply Now
@@ -55,16 +64,46 @@ const OpenRoles = () => {
           <div className="bg-white rounded-lg p-6 shadow-md">
             <h2 className="text-xl font-bold text-gray-800 mb-2">Cleaner</h2>
             <p className="text-gray-600">
-            Do you have an eye for detail and a passion for maintaining clean and organized spaces?
+              Do you have an eye for detail and a passion for maintaining clean
+              and organized spaces?
             </p>
             <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
               Apply Now
             </button>
           </div>
         </div>
-        <div className='w-full md:w-1/2 lg:w-1/3 px-4 mb-8'>
 
+        {/* Job 5 */}
+
+        <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+          <div className="bg-white rounded-lg p-6 shadow-md">
+            <h2 className="text-xl font-bold text-gray-800 mb-2">Painter</h2>
+            <p className="text-gray-600">
+              Are you skilled painter and can do magic while panting? 
+              Join us and share your creativity with our clients.
+            </p>
+            <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
+              Apply Now
+            </button>
+          </div>
         </div>
+
+
+      {/* Job 6 */}
+        <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+          <div className="bg-white rounded-lg p-6 shadow-md">
+            <h2 className="text-xl font-bold text-gray-800 mb-2">Carpenter</h2>
+            <p className="text-gray-600">
+              Do you have an eye for detail and a passion for maintaining clean
+              and organized spaces?
+            </p>
+            <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
+              Apply Now
+            </button>
+          </div>
+        </div>
+
+
       </div>
     </div>
   );
